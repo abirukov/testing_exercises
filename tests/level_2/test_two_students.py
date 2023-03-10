6a50bd74122980370a08e87fc17d1032c05db964
abirukov2008 <abirukov2008@gmail.com>
2023-03-10 09:06:32 +0300

    level2 мелкие правки 2

--- tests/level_2/test_two_students.py
+++ tests/level_2/test_two_students.py
@@ -4,12 +4,11 @@ from functions.level_2.two_students import get_student_by_tg_nickname, Student
 
 
 @pytest.mark.parametrize(
-    "telegram_username, students_list, expected",
+    "telegram_username, expected",
     [
-        ("ivan", pytest.lazy_fixture("students"), pytest.lazy_fixture("student_ivanov")),
-        ("petr", pytest.lazy_fixture("students"), None),
+        ("ivan", pytest.lazy_fixture("student_ivanov")),
+        ("petr", None),
     ],
 )
-def test__get_student_by_tg_nickname(telegram_username: str, students_list: list[Student], expected: Student):
-    print(students_list)
-    assert get_student_by_tg_nickname(telegram_username, students_list) == expected
+def test__get_student_by_tg_nickname(telegram_username: str, students: list[Student], expected: Student):
+    assert get_student_by_tg_nickname(telegram_username, students) == expected
